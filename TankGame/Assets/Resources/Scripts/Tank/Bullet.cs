4c24500f2225dafc0ac2a200360bc92d4fba2dcc
fvandepitte <frederiek.vandepitte@gmail.com>
2014-11-01 12:20:26 +0100

    added explosion

--- TankGame/Assets/Resources/Scripts/Tank/Bullet.cs
+++ TankGame/Assets/Resources/Scripts/Tank/Bullet.cs
@@ -3,8 +3,16 @@ using System.Collections;
 
 public class Bullet : MonoBehaviour {
 
+    public GameObject Smoke;
+
     public int TTL = 5;
+    public float MaxSpeed = 20F;
+    public float radius = 5.0F;
+    public float power = 10.0F;
+
     private float start;
+    private bool loaded;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,9 +22,29 @@ public class Bullet : MonoBehaviour {
 	// Update is called once per frame
 	void FixedUpdate () {
 
+        gameObject.rigidbody.velocity = Vector3.Lerp(gameObject.rigidbody.velocity, gameObject.rigidbody.transform.up * MaxSpeed, Time.fixedDeltaTime);
+
+        loaded = Time.time - start > 0.05f;
+
         if (start + TTL - Time.time < 0)
         {
             Destroy(gameObject);
         }
 	}
+
+    void OnCollisionEnter(Collision col) {
+        if (loaded) {
+            Instantiate(Smoke, transform.position, transform.rotation);
+            Vector3 explosionPos = transform.position;
+            Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+            foreach (Collider hit in colliders)
+            {
+                if (hit && hit.rigidbody)
+                    hit.rigidbody.AddExplosionForce(power, explosionPos, radius);
+
+            }
+
+            Destroy(gameObject);
+        }
+    }
 }
---
+++ TankGame/Assets/Resources/Scripts/Tank/Smoke.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Smoke : MonoBehaviour {
+    
+    public float TTL = 0.9F;
+    private float start;
+
+	// Use this for initialization
+	void Start () {
+        start = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (start + TTL - Time.time < 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
