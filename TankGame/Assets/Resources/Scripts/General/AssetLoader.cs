c5a8f86455f6fdac59fb907d00335fae1ae2fdc1
fvandepitte <frederiek.vandepitte@gmail.com>
2014-11-04 21:23:55 +0100

    added map functionality

--- TankGame/Assets/Resources/Scripts/General/AssetLoader.cs
+++ TankGame/Assets/Resources/Scripts/General/AssetLoader.cs
@@ -5,17 +5,29 @@ using SimpleJSON;
 
 public class AssetLoader
 {
-    private const string SPRITELOCATION = "Sprites/Terrain/";
+    private static AssetLoader _instance = new AssetLoader();
+
+    public static AssetLoader Instance {
+        get { return AssetLoader._instance; }
+    }
+
+    private const string KEYFILE = "MapFiles/key";
+
+    private const string SPRITELOCATION = "Sprites/Environment/";
     
     private JSONNode _root;
     private Dictionary<string, Sprite> _spritesMapping;
 
-    public AssetLoader() {
-        _root = JSON.Parse(Resources.Load<TextAsset>(string.Format("{0}{1}", SPRITELOCATION, "TileMapping")).text);
+    private AssetLoader() {
+        _root = JSON.Parse(Resources.Load<TextAsset>(KEYFILE).text);
         _spritesMapping = new Dictionary<string, Sprite>();
     }
 
     public Sprite Load(string name) {
+        if (!_spritesMapping.ContainsKey(name))
+        {
+            _spritesMapping[name] = Resources.Load<Sprite>(string.Format("{0}{1}", SPRITELOCATION, _root[name]["name"].Value));
+        }
         return _spritesMapping[name];
     }
 }
--- TankGame/Assets/Resources/Scripts/Map/Map.cs
+++ TankGame/Assets/Resources/Scripts/Map/Map.cs
@@ -5,11 +5,33 @@ using SimpleJSON;
 public class Map : MonoBehaviour {
 
     public TextAsset MapFile;
-
+    public GameObject[][] MapObjects;
 
 	// Use this for initialization
 	void Start () {
-	
+        if (MapFile)
+        {
+            JSONArray file = JSON.Parse(MapFile.text).AsArray;
+            MapObjects = new GameObject[file.Count][];
+
+            for (int row = 0; row < file.Count; row++)
+            {
+                JSONArray jrow = file[row].AsArray;
+                MapObjects[row] = new GameObject[jrow.Count];
+                for (int cell = 0; cell < jrow.Count; cell++)
+                {
+                    MapObjects[row][cell] = new GameObject(jrow[cell].Value);
+
+                    SpriteRenderer renderer = MapObjects[row][cell].AddComponent<SpriteRenderer>();
+                    renderer.sprite = AssetLoader.Instance.Load(jrow[cell].Value);
+
+                    MapObjects[row][cell].transform.Translate(new Vector3(cell * MapObjects[row][cell].renderer.bounds.size.x, -row * MapObjects[row][cell].renderer.bounds.size.y, 1));
+                    MapObjects[row][cell].transform.parent = transform;
+                }
+            }
+
+            
+        }
 	}
 	
 	// Update is called once per frame
